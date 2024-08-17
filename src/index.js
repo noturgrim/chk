2d2120cd907fbbc885262a80ccd41f2494a8fd23
noturgrim <130519012+noturgrim@users.noreply.github.com>
2024-08-17 18:33:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Toggle this variable to enable or disable maintenance mode
+const isMaintenance = true;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <App />
- 
-);
 
-
+if (isMaintenance) {
+  // Redirect to maintenance page
+  window.location.href = "/maintenance.html";
+} else {
+  root.render(<App />);
+}
